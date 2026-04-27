83054e9ac539158adcf7686699832aae59847c5c
ramunarlapati-13 <ramunarlapati@gmail.com>
2026-04-27 21:39:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'surepalli-pwa-v1';
+const CACHE_NAME = 'surepalli-pwa-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -14,6 +14,22 @@ self.addEventListener('install', event => {
         return cache.addAll(urlsToCache);
       })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
@@ -24,3 +40,4 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
